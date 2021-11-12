31eff1d71ebee9094c68eb8c5093b951c57c010f
JustinReed <jreed1@haverford.edu>
2021-11-12 16:55:48 -0500

    formatting of txt file 3.0

--- fileformatting.py
+++ fileformatting.py
@@ -1,11 +1,12 @@
 def word_character_list() -> None:
     file_handle = open('cmudict.txt', 'r')
     for line in file_handle:
-        modified_line = ""
-        for char in range(len(line[:-1])):
-            if line[char] + line[char + 1] != "  ":
-                modified_line += line[char]
+        current_line = line[:-1]
+        modified_line = current_line.replace("  ", " ")
         word_list = modified_line.split(" ")
+        print(word_list)
+
+
 
 
 
