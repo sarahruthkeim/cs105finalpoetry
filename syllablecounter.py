b0a4a822949aedbb16013d1be9f4b97561bd010b
pkulowiec <pkulowiec@haverford.edu>
2021-11-15 18:34:08 -0500

    -

--- syllablecounter.py
+++ syllablecounter.py
@@ -1,5 +1,5 @@
-from config import vowels
 from config import listofwords
+from config import vowels
 
 def syllable_counter(words:str)-> int:
     """
@@ -8,7 +8,7 @@ def syllable_counter(words:str)-> int:
     :return: The # of syllables in that word
     """
     #Creating string of all the vowels
-    #vowellist="AaEeIiOoUu"
+    #vowels="AaEeIiOoUu"
 
     #Creating a syllable count for the given word
     syllablecount=0
@@ -36,21 +36,17 @@ def syllable_counter(words:str)-> int:
     #Return the new syllable count
     #return syllablecount
     #print(syllablecount)
-
-
     for index in range(len(listofwords)):
-        if listofwords[index][0] == words:
-            print("Hello")
+        if listofwords[index][0]==words:
             for i in listofwords[index]:
-                if i in vowels:
+                if i[0:2] in vowels:
                     syllablecount+=1
+            #return(syllablecount)
             print(syllablecount)
 
-# .......
-syllable_counter('sparkle')
-syllable_counter('submarine')
-syllable_counter('ate')
-syllable_counter('crucial')
-syllable_counter('preach')
-syllable_counter('preacher')
-syllable_counter('antelope')
+syllable_counter('SPARKLE')
+syllable_counter('SUBMARINE')
+syllable_counter('ATE')
+syllable_counter('CRUCIAL')
+syllable_counter('PREACH')
+syllable_counter('PREACHER')
