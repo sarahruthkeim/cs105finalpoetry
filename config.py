2e47b901ef3273c7664ac09e9b732fbec8c9044a
JustinReed <jreed1@haverford.edu>
2021-11-15 17:28:36 -0500

    rhyme function is a bit broken, coming back to it later

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ from fileformatting import word_character_list
 
 listofwords = word_character_list()
 
+
--- justinsavefile.py
+++ justinsavefile.py
@@ -15,3 +15,37 @@ def find_word(list_of_lists, user_word: str) -> list:
             return list_of_lists[line_list]
 
 print(find_word(test_list, 'WORD'))
+
+
+# This is the old version of perfect_rhyme()
+
+def perfect_vowel(word1: str, word2: str) -> bool:
+    vowels = ["AA", "AE", "AH", "AO", "AW", "AY", "EH", "ER", "EY", "IH", "IY", "UH", "UW", "OW"]
+    broken_word1 = convert_word(word1)
+    broken_word2 = convert_word(word2)
+    if broken_word1 == [0] or broken_word2 == [0]:
+        raise Exception
+    else:
+        if len(broken_word1) != len(broken_word2):
+            return False
+        else:
+            current_word1_vowel: str = ""
+            current_word2_vowel: str = ""
+            while current_word1_vowel == "" or current_word2_vowel == "":
+                longest_word = max(len(broken_word1), len(broken_word2))
+                for syllable_index in range(1, longest_word):
+                    current_word1_syllable = broken_word1[syllable_index][0:2]
+                    current_word2_syllable = broken_word2[syllable_index][0:2]
+                    if current_word1_syllable in vowels:
+                        current_word1_vowel = current_word1_syllable
+                        print(current_word1_vowel)
+                    if current_word2_syllable in vowels:
+                        current_word2_vowel = current_word2_syllable
+                        print(current_word2_vowel)
+                    if current_word1_vowel != "" and current_word2_vowel != "":
+                        if current_word1_vowel != current_word2_vowel:
+                            return False
+                        else:
+                            current_word1_syllable = ""
+                            current_word2_syllable = ""
+            return True
