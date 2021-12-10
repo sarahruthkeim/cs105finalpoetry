537cff90c338102481d5ef9df96ab0c11f4ffa6e
pkulowiec <pkulowiec@haverford.edu>
2021-12-10 14:58:41 -0500

    -

--- displaywindow.py
+++ displaywindow.py
@@ -29,7 +29,6 @@ def StyleIndentifier():
         newWindow.geometry("200x100")
         Label(newWindow, text=poem_type()).pack()
 
-
 def SyllableCounter():
     newWindow = Toplevel(window)
     newWindow.title("Syllable Counter")
